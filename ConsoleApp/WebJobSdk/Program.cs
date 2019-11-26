4a29a4011594e883b8253efe47631b8b41d92cfa
rameshksh <ramesh.sharma12@gmail.com>
2019-11-26 12:51:20 +0530

    test project

---
+++ ConsoleApp/WebJobSdk/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace WebJobSdk
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
